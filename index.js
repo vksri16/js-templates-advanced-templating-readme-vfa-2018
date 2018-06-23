cfeae185a7911ee4fb8c7eb4f9bd69426fd59bd6
Vidisha Srivastav <vidisha.srivastav16@gmail.com>
2018-06-23 10:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function loadIssue() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for(var i=0; i<issues.length; i++) {
+  for(var i=0;i<issues.length;i++) {
     var result = template(issues[i]);
+    document.getElementsByTagName("main")[0].innerHTML += result;
   }
-  document.getElementsByTagName("main")[0].innerHTML+=result;
 }
