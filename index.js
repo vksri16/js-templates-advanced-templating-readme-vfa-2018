ae27cf30c1fc1fe80ecd5645ed5c0f1a0bf6d851
Vidisha Srivastav <vidisha.srivastav16@gmail.com>
2018-06-23 10:47:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state==="closed") {
+    return new
+  }
+})
+
 function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
     var result = template(issues);
